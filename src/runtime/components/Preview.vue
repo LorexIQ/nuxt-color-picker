ebf3852e8fee395cb50e0d701e43b6417a9ab604
LorexIQ <i@liq-mail.ru>
2024-12-17 13:01:09 +0300

    feat: добавлена возможность выбора начального цвета нажатием по нему

--- src/runtime/components/Preview.vue
+++ src/runtime/components/Preview.vue
@@ -4,10 +4,14 @@ import { computed, onMounted, ref, watch } from '#imports';
 
 type Props = {
   color: string;
-  oldColor: string;
+  oldColor?: string;
+};
+type Emits = {
+  (e: 'select', v: string): void;
 };
 
 const props = defineProps<Props>();
+const emit = defineEmits<Emits>();
 
 const containerRef = ref<HTMLHtmlElement>();
 const containerCanvasRef = ref<HTMLCanvasElement>();
@@ -27,11 +31,15 @@ function renderColor() {
   ctx.fillStyle = ctx.createPattern(canvasSquare, 'repeat')!;
   ctx.fillRect(0, 0, width.value, height.value);
 
-  ctx.fillStyle = props.color;
-  ctx.fillRect(0, 0, width.value / 2, height.value);
-
-  ctx.fillStyle = props.oldColor;
-  ctx.fillRect(width.value / 2, 0, width.value / 2, height.value);
+  if (props.oldColor) {
+    ctx.fillStyle = props.color;
+    ctx.fillRect(0, 0, Math.floor(width.value / 2), height.value);
+    ctx.fillStyle = props.oldColor;
+    ctx.fillRect(Math.floor(width.value / 2), 0, Math.ceil(width.value / 2), height.value);
+  } else {
+    ctx.fillStyle = props.color;
+    ctx.fillRect(0, 0, width.value, height.value);
+  }
 }
 
 onMounted(() => {
@@ -45,5 +53,25 @@ onMounted(() => {
     class="CP-preview CP--box-border"
   >
     <canvas ref="containerCanvasRef" />
+    <div
+      v-if="oldColor"
+      class="CP-preview__old"
+      @click="emit('select', oldColor)"
+    />
   </div>
 </template>
+
+<style lang="scss" scoped>
+.CP-preview {
+  position: relative;
+
+  &__old {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 50%;
+    cursor: pointer;
+  }
+}
+</style>
